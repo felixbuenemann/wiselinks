635f2c61046c81ed67d2af719a8d0e24e76e022e
Felix Bünemann <felix.buenemann@gmail.com>
2024-10-02 15:44:16 +0200

    Fix Ruby 2.7 URI.encode deprecation warnings

--- lib/wiselinks/controller_methods.rb
+++ lib/wiselinks/controller_methods.rb
@@ -24,48 +24,54 @@ module Wiselinks
     def wiselinks_title(value)
       if self.request.wiselinks? && value.present?
         Wiselinks.log("title: #{value}")
-        self.response.headers['X-Wiselinks-Title'] = URI.encode(value)
+        self.response.headers['X-Wiselinks-Title'] = uri_encode(value)
       end
     end
 
     def wiselinks_description(value)
       if self.request.wiselinks? && value.present?
         Wiselinks.log("description: #{value}")
-        self.response.headers['X-Wiselinks-Description'] = URI.encode(value)
+        self.response.headers['X-Wiselinks-Description'] = uri_encode(value)
       end
     end
 
     def wiselinks_canonical(value)
       if self.request.wiselinks? && value.present?
         Wiselinks.log("canonical: #{value}")
-        self.response.headers['X-Wiselinks-Canonical'] = URI.encode(value)
+        self.response.headers['X-Wiselinks-Canonical'] = uri_encode(value)
       end
     end
 
     def wiselinks_robots(value)
       if self.request.wiselinks? && value.present?
         Wiselinks.log("robots: #{value}")
-        self.response.headers['X-Wiselinks-Robots'] = URI.encode(value)
+        self.response.headers['X-Wiselinks-Robots'] = uri_encode(value)
       end
     end
 
     def wiselinks_link_rel_prev(value)
       if self.request.wiselinks? && value.present?
         Wiselinks.log("link_rel_prev: #{value}")
-        self.response.headers['X-Wiselinks-LinkRelPrev'] = URI.encode(value)
+        self.response.headers['X-Wiselinks-LinkRelPrev'] = uri_encode(value)
       end
     end
 
     def wiselinks_link_rel_next(value)
       if self.request.wiselinks? && value.present?
         Wiselinks.log("link_rel_next: #{value}")
-        self.response.headers['X-Wiselinks-LinkRelNext'] = URI.encode(value)
+        self.response.headers['X-Wiselinks-LinkRelNext'] = uri_encode(value)
       end
     end
 
     def set_wiselinks_url
       # self.response.headers['X-Wiselinks-Url'] = request.env['REQUEST_URI'] if self.request.wiselinks?
       self.response.headers['X-Wiselinks-Url'] = request.url if self.request.wiselinks?
+    end
+
+  private
+
+    def uri_encode(value)
+      URI::DEFAULT_PARSER.escape(value)
     end
   end
 end
