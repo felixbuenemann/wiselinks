84fe1182171fa497acfdf82d15df79fc64c2eda9
Felix Bünemann <felix.buenemann@gmail.com>
2025-01-20 18:37:38 +0100

    Fix calculation of asset digest on Rails 5+
    by checking for sprockets and propshaft manifest files.

--- lib/wiselinks/rails.rb
+++ lib/wiselinks/rails.rb
@@ -13,8 +13,16 @@ module Wiselinks
       end
 
       initializer "wiselinks.register_assets_digest"  do
-        if ::Rails.application.config.assets.digest && ::Rails.application.config.assets.digests.present?
-          Wiselinks.options[:assets_digest] ||= Digest::MD5.hexdigest(::Rails.application.config.assets.digests.values.join)
+        if ::Rails.application.config.assets.digest
+          if ::Rails.application.config.assets.digests.present?
+            Wiselinks.options[:assets_digest] ||= Digest::MD5.hexdigest(::Rails.application.config.assets.digests.values.join)
+          elsif ::Rails.application.config.assets.manifest.present? && File.exist?(::Rails.application.config.assets.manifest)
+            Wiselinks.options[:assets_digest] ||= Digest::MD5.hexdigest(File.read(::Rails.application.config.assets.manifest))
+          elsif manifest = ::Rails.root.join('public', 'assets').glob('.sprockets-manifest-*.json').map(&:mtime).last
+            Wiselinks.options[:assets_digest] ||= Digest::MD5.hexdigest(manifest.read)
+          elsif manifest = ::Rails.root.join('public', 'assets', '.manifest.json') and manifest.exist?
+            Wiselinks.options[:assets_digest] ||= Digest::MD5.hexdigest(manifest.read)
+          end
         end
       end
     end
